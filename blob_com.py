dc7f586459fd9698ba81d51cfe1fd5c60fa1b879
Ben Dudson <bd512@york.ac.uk>
2018-06-12 16:48:18 +0100

    Meant to check in this version

--- blob_com.py
+++ blob_com.py
@@ -2,8 +2,7 @@
 # Centre-of-mass tracking of plasma blob.
 # Calculates R, Z, psi as a function of time on a given toroidal slice (y index)
 
-gridfile = "tokamak-ny10.fci.nc"
-datapath = "filament-200x10x200/100eV"
+datasets = [("filament", "tokamak-ny10.fci.nc", "100eV")]
 yindex = 5
 
 from boutdata import collect
@@ -11,34 +10,47 @@ from boututils.datafile import DataFile
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Read values from the grid file
-with DataFile(gridfile) as g:
-    R = g["R"][:,yindex,:]
-    Z = g["Z"][:,yindex,:]
-    #psi = g["psi"][:,yindex,:]
+for datapath, gridfile, label in datasets:
+
+    # Read values from the grid file
+    with DataFile(gridfile) as g:
+        R = g["R"][:,yindex,:]
+        Z = g["Z"][:,yindex,:]
+        #psi = g["psi"][:,yindex,:]
     
-
-# Read the density
-n = collect("n", yind=yindex, path=datapath)[:,:,0,:]
-
-time = collect("t_array", path=datapath)
-
-nt = n.shape[0]
-
-# Arrays to contain the R, Z and psi coordinates of the blob
-Rcom = np.ndarray(nt)
-Zcom = np.ndarray(nt)
-#psicom = np.ndarray(nt)
-
-for t in range(nt):
-    n_cell = n[t,:,:] * R # Note: Volume of cell proportional to R
-    ntot = np.sum(n_cell) 
-    Rcom[t] = np.sum(n_cell * R) / ntot
-    Zcom[t] = np.sum(n_cell * Z) / ntot
-    #psicom[t] = np.sum(n_cell * psi) / ntot
     
-plt.plot(time, Rcom)
+    # Read the density
+    n = collect("n", yind=yindex, path=datapath)[:,:,0,:]
+    
+    time = collect("t_array", path=datapath)
+    wci = collect("Omega_ci", path=datapath)
+    time *= 1e6 / wci # microseconds
+    
+    nt = n.shape[0]
+    
+    # Arrays to contain the R, Z and psi coordinates of the blob
+    Rcom = np.ndarray(nt)
+    Zcom = np.ndarray(nt)
+    #psicom = np.ndarray(nt)
+    
+    for t in range(nt):
+        n_cell = n[t,:,:] * R # Note: Volume of cell proportional to R
+        ntot = np.sum(n_cell) 
+        Rcom[t] = np.sum(n_cell * R) / ntot
+        Zcom[t] = np.sum(n_cell * Z) / ntot
+        #psicom[t] = np.sum(n_cell * psi) / ntot
+
+    Rcom *= 1e3 # mm
+        
+    plt.plot(time - time[0], Rcom - Rcom[0], label=label)
+
+plt.xlabel(r"Time $\mu s$")
+plt.ylabel("Major radius [mm]")
+plt.legend()
+
+plt.savefig("blob_com.png")
+plt.savefig("blob_com.pdf")
+
 plt.show()
 
-plt.plot(time, Zcom)
-plt.show()
+
